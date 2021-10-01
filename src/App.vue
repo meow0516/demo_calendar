c3ca76d837d1405dc4448ee496ab1022ffd44292
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-10-01 14:02:41 +0800

    modify calendar structure: add calendar, google login

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        Demo_Calendar
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      <GoogleLogin :params="params" :renderParams="renderParams" :onSuccess="onSuccess">login</GoogleLogin>
+      <calendar/>
+    </v-main>
+  </v-app>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// import Signin from './components/signin.vue'
+import GoogleLogin from 'vue-google-login';
+import Calendar from './components/calendar.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    // Signin,
+    GoogleLogin,
+    Calendar,
+  },
+
+  data: () => ({
+    params: {
+      client_id: "642199378747-62svnibd4medlgj1ptd685ol8vf5l322.apps.googleusercontent.com"
+    },
+    renderParams: {
+      width: 250,
+      height: 50,
+      longtitle: true
+    }
+  }),
+
+  methods: {
+    onSuccess(googleUser) {
+      console.log(googleUser);
+      console.log(googleUser.getBasicProfile());
+    },
+  },
+};
+</script>
