1890caea42e0a3000e2173764293cc72869c56f0
meow0516 <76482269+meow0516@users.noreply.github.com>
2021-10-22 16:45:35 +0800

    set vuex, gapi, vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import VueGapi from 'vue-gapi'
+// import GAuth from 'vue-google-oauth2'
+import moment from 'moment'
 
+
+// const gauthOption = {
+//   clientId: '642199378747-62svnibd4medlgj1ptd685ol8vf5l322.apps.googleusercontent.com',
+//   scope: 'profile email',
+//   prompt: 'select_account'
+// }
+
+// Vue.use(GAuth, gauthOption)
+Vue.prototype.moment = moment
 Vue.config.productionTip = false
 
+Vue.use(VueGapi, {
+  apiKey: 'AIzaSyBtALxqy0fusAShU5n7ecYoCQwcrNUxKwY',
+  clientId: '642199378747-62svnibd4medlgj1ptd685ol8vf5l322.apps.googleusercontent.com',
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
+  scope: 'https://www.googleapis.com/auth/calendar',
+})
+
 new Vue({
-  render: h => h(App),
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
+
+
